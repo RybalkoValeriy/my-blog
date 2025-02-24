6494d0ac67a26c40309551be894f6a1c8eee3523
RybalkoValerii <typeonegative86@gmail.com>
2025-02-24 20:52:12 +0200

    fix

--- config/site.ts
+++ config/site.ts
@@ -2,13 +2,13 @@ import { Linkedin } from 'lucide-react'
 
 export const siteConfig = {
   name: 'Valerii Rybalko’s Blog',
-  url: 'https://example.com', // todo: change it?
+  url: 'https://val86.site',
   description: 'Nextjs 14 blog using velite, tailwind and shadcn',
   author: 'Valerii Rybalko',
   links: {
     linkedin: 'https://www.linkedin.com/in/valerii-rybalko/',
     github: 'https://github.com/RybalkoValeriy',
-    personalSite: 'https://valrii.com', // todo: change it?
+    personalSite: 'https://val86.site',
     email: 'typeonegative86@gmail.com',
   },
 }
